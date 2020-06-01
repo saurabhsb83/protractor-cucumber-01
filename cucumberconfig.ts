de371fee0f5817be0c913cfd5bf8325b561e0084
Saurabh <saurabh.sb83@gmail.com>
2020-06-01 07:58:26 +0530

    Latest version checked in

--- cucumberconfig.ts
+++ cucumberconfig.ts
@@ -24,7 +24,7 @@ export let config: Config = {
 
     cucumberOpts: {
         
-        tags: "@CalculatorTesting",
+        //tags: "@CalculatorTesting",
         format: 'json:./cucumberreport.json',
         require: [
           './stepDefinitions/*.js' 
